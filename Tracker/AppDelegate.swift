40ac7ad5891e3971730527729b9fc8c20a6991e3
Denis Beliakov <ddenn@outlook.com>
2023-12-16 16:46:46 +0300

    initial commit

--- Tracker/AppDelegate.swift
+++ Tracker/AppDelegate.swift
@@ -1,20 +1,16 @@
-//
-//  AppDelegate.swift
-//  Tracker
-//
-//  Created by Денис Беляков on 16.12.2023.
-//
-
 import UIKit
 import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow()
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
         return true
     }
 
